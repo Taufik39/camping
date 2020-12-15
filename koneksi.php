38aea4562b08657b64e8fa3b51bc6d47fe0a0fbd
Taufik Nurhidayat <taufiknurhidayat39@gmail.com>
2020-12-15 11:32:26 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,7 @@
 <?php 
 try { 
-    $pdo_conn = new PDO('mysql:host=remotemysql.com;dbname=2zmvexNt83', '2zmvexNt83', 'AtaCsX7KJz'); 
+    $pdo_conn = new PDO('mysql:host=remotemysql.com;dbname=2zmvexNt83', '2zmvexNt83', 'AtaCsX7KJz',
+    array(PDO::ATTR_PERSISTENT => true)); 
 } 
 catch(PDOException $e) { echo $e->getMessage(); 
 } 
