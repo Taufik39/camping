c0484467bfbfd566a992bc483e829cc827ed80bd
Taufik Nurhidayat <taufiknurhidayat39@gmail.com>
2020-12-17 03:13:07 +0700

    cari bug

--- tambahakun.php
+++ tambahakun.php
@@ -4,7 +4,8 @@ if(!empty($_POST["daftar"])) {
 	require_once("koneksi.php");
 	$sql = "INSERT INTO bio_penyewa (nama_penyewa, username_penyewa, pass_penyewa, email_penyewa, no_hp_p, alamat_penyewa) VALUES (:nama, :username, :pass, :email, :nohp, :alamat)";
 	$stmt = $pdo_conn->prepare( $sql );
-	$result = $stmt->execute( array(':nama'=>$_POST['nama'],':username'=>$_POST['username'], ':pass'=>$_POST['pass'], ':email'=>$_POST['email'], ':nohp'=>$_POST['nohp'], ':alamat'=>$_POST['alamat'] ) );
+    $result = $stmt->execute( array
+    (':nama'=>$_POST['nama'],':username'=>$_POST['username'], ':pass'=>$_POST['pass'], ':email'=>$_POST['email'], ':nohp'=>$_POST['nohp'], ':alamat'=>$_POST['alamat'] ) );
 	if (!empty($result) ){
       header('location:masukuser.php');
       $insertMsg="Akun berhasil dibuat!"; 
--- tambahakunmitra.php
+++ tambahakunmitra.php
@@ -4,7 +4,8 @@ if(!empty($_POST["daftarmitra"])) {
 	require_once("koneksi.php");
 	$sql = "INSERT INTO bio_menyewakan (nama_menyewakan, username, pass_menyewakan, email, no_hp, nama_toko, alamat, profil) VALUES (:nama, :username, :pass, :email, :nohp, :mitra, :alamat, :profil)";
 	$stmt = $pdo_conn->prepare( $sql );
-	$result = $stmt->execute( array(':nama'=>$_POST['nama'],':username'=>$_POST['username'], ':pass'=>$_POST['pass'], ':email'=>$_POST['email'], ':nohp'=>$_POST['nohp'], ':mitra'=>$_POST['mitra'] , ':alamat'=>$_POST['alamat'] , ':profil'=>$_POST['profil'] ) );
+    $result = $stmt->execute( array 
+    (':nama'=>$_POST['nama'],':username'=>$_POST['username'], ':pass'=>$_POST['pass'], ':email'=>$_POST['email'], ':nohp'=>$_POST['nohp'], ':mitra'=>$_POST['mitra'] , ':alamat'=>$_POST['alamat'] , ':profil'=>$_POST['profil'] ) );
 	if (!empty($result) ){
       header('location:masukmitra.php');
       $insertMsg="Akun berhasil dibuat!"; 
