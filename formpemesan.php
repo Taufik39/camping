3395a02fb8cc1b94f2ffeba3e11d5943cd576f4a
Taufik Nurhidayat <taufiknurhidayat39@gmail.com>
2020-12-24 23:39:08 +0700

    utak atik session nama barang

--- formpemesan.php
+++ formpemesan.php
@@ -66,6 +66,9 @@ a,a:hover,a:visited,a:active{
        
     <?php
         require_once("koneksi.php");
+        $stmt = $pdo_conn->prepare("SELECT * FROM barang JOIN bio_menyewakan JOIN bio_penyewa WHERE nama_barang= :nama_barang ");
+        $stmt->execute(array(':nama_barang' => $_SESSION['nama_barang']));
+        $result = $stmt->fetchAll();
     ?>
            <div class="login-body">
            <form name="frmAdd" action="tambahpesanan.php" method="POST" enctype="multipart/form-data">
@@ -80,13 +83,13 @@ a,a:hover,a:visited,a:active{
                         <input type="text" name="jumlah" required placeholder="Jumlah" />
                     </div>
                     <div class="form-group">
-                        <input type="text" name="barang" value="<?php echo $cetakan[0]['id_barang']; ?>" readonly=""/>
+                        <input type="text" name="barang" value="<?php echo $result[0]['id_barang']; ?>" readonly=""/>
                     </div>
                     <div class="form-group">
-                        <input type="text" name="pemilik" value="<?php echo $cetakan[0]['id_menyewakan']; ?>" readonly=""/>
+                        <input type="text" name="pemilik" value="<?php echo $result[0]['id_menyewakan']; ?>" readonly=""/>
                     </div>
                     <div class="form-group">
-                        <input type="text" name="penyewa" value="<?php echo $cetakan[0]['id_penyewa']; ?>" readonly=""/>
+                        <input type="text" name="penyewa" value="<?php echo $result[0]['id_penyewa']; ?>" readonly=""/>
                     </div>
                         <input type="submit" class="btn btn-block btn-custom-pink" value="PESAN" name="tambahpesanan"/>
                     
